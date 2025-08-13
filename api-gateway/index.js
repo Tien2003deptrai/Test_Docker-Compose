53a0607978eff1406effa88f7bb7cb85e4d88c82
Tien2003deptrai <duongvantiendtu@gmail.com>
2025-08-13 07:41:59 +0700

    done task api-gateway

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,8 +1,8 @@
 // text branch
 // feature2 branch
-console.log("Xin chào thế giới");
+console.log('Xin chào thế giới')
 
 // feature11 branch 3333
-console.log("Hello World");
+console.log('Hello World')
 
-
+console.log('T muốn phá m lắm, ngu chết nè con')
